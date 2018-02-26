7c879ca22cf9b277465aaee7504d2199950be76a
Clint Thacker <couscous@gmail.com>
2018-02-26 15:28:37 -0600

    Store changes in order to go back and integrate Flow

--- js/App.jsx
+++ js/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import type { Match } from 'react-router-dom';
-import Landing from './Landing.jsx';
-import Search from './Search.jsx';
-import Details from './Details.jsx';
+import Landing from './Landing';
+import Search from './Search';
+import Details from './Details';
 import preload from '../data.json';
 
 const FourOhFour = () => <h1>404</h1>;
@@ -14,14 +14,14 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
-        {/*        <Route
+        <Route
           path="/details/:id"
           component={props => {
             const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
             return <Details show={selectedShow} />;
           }}
-        />*/}
-        <Route component={Details} />
+        />
+        <Route path="/details/:id" component={Details} />
         <Route component={FourOhFour} />
       </Switch>
     </div>
