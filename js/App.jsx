2b692c8be86c2e728f015ca97cd0ebe9f54922f3
Clint Thacker <couscous@gmail.com>
2018-02-26 14:16:03 -0600

    Changes which aren't working

--- js/App.jsx
+++ js/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Landing from './Landing';
-import Search from './Search';
+import type { Match } from 'react-router-dom';
+import Landing from './Landing.jsx';
+import Search from './Search.jsx';
+import Details from './Details.jsx';
+import preload from '../data.json';
 
 const FourOhFour = () => <h1>404</h1>;
 
@@ -11,6 +14,14 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
+        {/*        <Route
+          path="/details/:id"
+          component={props => {
+            const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
+            return <Details show={selectedShow} />;
+          }}
+        />*/}
+        <Route component={Details} />
         <Route component={FourOhFour} />
       </Switch>
     </div>
